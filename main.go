5b577991aa07ac97c0900717994457a814624707
Marsel Garipov <garipov.marsel@wb.ru>
2023-11-24 12:44:28 +0300

    fix panic

--- main.go
+++ main.go
@@ -3,19 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
-	"math/rand"
-
 	"golang.design/x/clipboard"
+	"log"
+	"math/rand"
+	"strconv"
 )
 
 var (
 	symbolsSpec = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=")
 	symbols     = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
-)
-
-const (
-	defaultPassLen         = 64
-	defaultCopyToClipboard = true
 )
 
 func main() {
@@ -24,32 +20,42 @@ func main() {
 	}
 
 	var (
-		passLen         = defaultPassLen
-		copyToClipboard = defaultCopyToClipboard
+		passLen            = 64
+		copyToClipboard    = true
+		withoutSpecSymbols = true
 
-		withoutSpecSymbols bool
+		err error
 	)
 
-	l := flag.Int("l", defaultPassLen, "# len of password")
-	c := flag.Bool("c", defaultCopyToClipboard, "# copy to clipboard: false or true")
-	s := flag.Bool("s", withoutSpecSymbols, "# without spec symbols: false or true")
+	l := flag.Int("l", passLen, "# len of password")
+	c := flag.String("c", fmt.Sprint(copyToClipboard), "# copy to clipboard: false or true")
+	s := flag.String("s", fmt.Sprint(withoutSpecSymbols), "# without spec symbols: false or true")
 	flag.Parse()
 
 	if l != nil {
 		passLen = *l
 	}
+	if s != nil {
+		withoutSpecSymbols, err = strconv.ParseBool(*s)
+		if err != nil {
+			log.Fatalf("cannot parse param 's' - without spec symbols, err: %v", err)
+		}
+	}
 	if c != nil {
-		copyToClipboard = *c
+		copyToClipboard, err = strconv.ParseBool(*c)
+		if err != nil {
+			log.Fatalf("cannot parse param 'c' - copy to clipboard, err: %v", err)
+		}
 	}
 
-	b := make([]rune, passLen)
-	if s != nil && *s {
-		for i := range b {
-			b[i] = symbols[rand.Intn(len(symbolsSpec))]
+	b := make([]rune, 0, passLen)
+	if withoutSpecSymbols {
+		for i := 0; i < passLen; i++ {
+			b = append(b, symbolsSpec[rand.Intn(len(symbolsSpec))])
 		}
 	} else {
-		for i := range b {
-			b[i] = symbols[rand.Intn(len(symbols))]
+		for i := 0; i < passLen; i++ {
+			b = append(b, symbols[rand.Intn(len(symbols))])
 		}
 	}
 	pass := string(b)
@@ -57,6 +63,6 @@ func main() {
 	if copyToClipboard {
 		clipboard.Write(clipboard.FmtText, []byte(pass))
 	}
-	fmt.Printf("Success generate password with len: %v, copy to clipboard: %v\n", passLen, copyToClipboard)
+	fmt.Printf("Success generate password with len: %v; without spec symbols: %v; copy to clipboard: %v\n", passLen, withoutSpecSymbols, copyToClipboard)
 	fmt.Println(pass)
 }
